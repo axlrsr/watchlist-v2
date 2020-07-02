fc6461a12fb51e345d30cdbc46429d36e1666dd8
AXLRSR <62110917+AXLRSR@users.noreply.github.com>
2020-07-02 18:09:57 +0200

    ✨ Introducing movie details (+ patches)

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -3,7 +3,8 @@ import NoImage from '../images/no-image.jpg'
 
 class Movie extends Component {
     state = {
-        movie: {}
+        movie: {},
+        display: false
     }
 
     componentDidMount(){
@@ -14,16 +15,48 @@ class Movie extends Component {
             })
     }
 
+    toggleDisplay = () => {
+        this.setState({display: !this.state.display});
+    }
+
+    displayDetails = () => {
+        if(this.state.display) {
+            return <div className="details">
+                <div className="details__popup">
+                    <div className="details__center">
+                        <div className="details__close" onClick={this.toggleDisplay}></div>
+                    </div>
+                    <h3 className="details__title">{this.state.movie.title}</h3>
+                    <p className="details__info">{this.state.movie.release_date}</p>
+                    <p className="details__subtitle">Overview</p>
+                    <p className="details__info">
+                        {this.state.movie.genres.map((genre, index) => {
+                            if(index !== this.state.movie.genres.length - 1) {
+                                return <span>{genre.name} • </span>
+                            } else {
+                                return <span>{genre.name}</span>
+                            }
+                        })}
+                    </p>
+                    <p className="details__overview">{this.state.movie.overview}</p>
+                </div>
+            </div>
+        }
+    }
+
     displayPoster = () => {
         if(this.state.movie.poster_path) {
-            return <img className="grid__poster" src={`http://image.tmdb.org/t/p/w200${this.state.movie.poster_path}`} alt=""/>
+            return <img className="grid__img" src={`http://image.tmdb.org/t/p/w200${this.state.movie.poster_path}`} alt="" onClick={this.toggleDisplay} />
         } else {
-            return <img className="grid__poster" src={NoImage} alt=""/>
+            return <img className="grid__img" src={NoImage} alt="" onClick={this.toggleDisplay} />
         }
     }
 
     render() {
-        return this.displayPoster()
+        return <div className="grid__poster">
+            {this.displayPoster()}
+            {this.displayDetails()}
+        </div>
     }
 }
 
