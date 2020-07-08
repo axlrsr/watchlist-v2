206cd8b41222b08ec3a28efe18b22e6bacda6648
AXLRSR <62110917+AXLRSR@users.noreply.github.com>
2020-07-08 16:07:54 +0200

    ✨ Introducing "My list"

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -4,7 +4,8 @@ import NoImage from '../images/no-image.jpg'
 class Movie extends Component {
     state = {
         movie: {},
-        display: false
+        display: false,
+        value: null
     }
 
     componentDidMount(){
@@ -14,6 +15,47 @@ class Movie extends Component {
                 this.setState({movie: result})
             })
     }
+
+    addMovie = () => {
+        localStorage.setItem(this.state.movie.id, false);
+        this.setState({value: false})
+    }
+
+    removeMovie = () => {
+        localStorage.removeItem(this.state.movie.id);
+        this.setState({value: null})
+    }
+
+    viewedMovie = () => {
+        localStorage.setItem(this.state.movie.id, true);
+        this.setState({value: true})
+    }
+
+    notViewedMovie = () => {
+        localStorage.setItem(this.state.movie.id, false);
+        this.setState({value: false})
+    }
+
+    displayBtn = () => {
+        if (localStorage.getItem(this.state.movie.id) === null) {
+            return <div className="btn__container">
+                <button className="btn btn--primary" onClick={this.addMovie}>ADD</button>
+                <button className="btn btn--primary" onClick={this.viewedMovie}>VIEWED</button>
+            </div>
+        }
+        if (localStorage.getItem(this.state.movie.id) === 'false') {
+            return <div className="btn__container">
+                <button className="btn btn--secondary" onClick={this.removeMovie}>REMOVE</button>
+                <button className="btn btn--primary" onClick={this.viewedMovie}>VIEWED</button>
+            </div>
+        }
+        if (localStorage.getItem(this.state.movie.id) === 'true') {
+            return <div className="btn__container">
+                <button className="btn btn--secondary" onClick={this.removeMovie}>REMOVE</button>
+                <button className="btn btn--secondary" onClick={this.notViewedMovie}>NOT VIEWED</button>
+            </div>
+        }
+    } 
 
     toggleDisplay = () => {
         this.setState({display: !this.state.display});
@@ -32,13 +74,15 @@ class Movie extends Component {
                     <p className="details__info">
                         {this.state.movie.genres.map((genre, index) => {
                             if(index !== this.state.movie.genres.length - 1) {
-                                return <span>{genre.name} • </span>
+                                return <span key={genre.id}>{genre.name} • </span>
                             } else {
-                                return <span>{genre.name}</span>
+                                return <span key={genre.id}>{genre.name}</span>
                             }
                         })}
                     </p>
                     <p className="details__overview">{this.state.movie.overview}</p>
+                    
+                    {this.displayBtn()}
                 </div>
             </div>
         }
--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,9 +1,40 @@
 import React, {Component} from 'react';
 
+import Movie from '../components/Movie';
+
 class List extends Component {
+    state = {
+        notViewed: [],
+        viewed: []
+    }
+
+    componentDidMount() {
+        const notViewed = []
+        const viewed = []
+
+        Object.keys(localStorage).map((key) => {
+            if (localStorage[key] === 'false') {
+                notViewed.push(key)
+            } else if (localStorage[key] === 'true') {
+                viewed.push(key)
+            }
+        })
+
+        this.setState({notViewed})
+        this.setState({viewed})
+    }
+
     render() {
         return <div className="content">
-            <h1>List</h1>
+            <h2 className="content__title">Not viewed</h2>
+            <div className="grid">
+                {this.state.notViewed.map(id => (<Movie key={id} id={id} />))}
+            </div>
+
+            <h2 className="content__title">Viewed</h2>
+            <div className="grid">
+                {this.state.viewed.map(id => (<Movie key={id} id={id} />))}
+            </div>
         </div>
     }
 }
